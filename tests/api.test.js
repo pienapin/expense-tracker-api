de202c4f15b20a83e25cd9a365cf2638852899bf
pien <pincenna@gmail.com>
2022-11-07 20:19:44 +0700

    menambahkan test untuk API

---
+++ tests/api.test.js
@@ -0,0 +1,103 @@
+const request = require('supertest');
+const app = require('./server')
+
+afterAll((done) => {
+  done();
+});
+
+describe('User API', () => {
+  test('List User', async () => {
+    const res = await request(app).get('/api/user/all');
+    expect(res.status).toBe(200);
+  });
+
+  test('Add User', async () => {
+    const res = await request(app).post('/api/user/register').send({
+      "name": "add user test",
+      "email": "test@test.com",
+      "password": "test"
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('User Login', async () => {
+    const res = await request(app).post('/api/user/login').send(
+    {
+      "email": "test@test.com",
+      "password": "test"
+    });
+    token = res.body.token;
+    expect(res.status).toBe(200);
+  });
+
+  test('User Logout', async () => {
+    const res = await request(app).post('/api/user/logout')
+    expect(res.status).toBe(200);
+  });
+
+  test('Edit User', async() => {
+    const login = await request(app).post('/api/user/login').send(
+    {
+      "email": "test@test.com",
+      "password": "test"
+    });
+    token = login.body.token;
+    const res = await request(app).put('/api/user/edit').set('Cookie', [`token=${token}`]).send(
+    {
+      "name": "tes ganti nama",
+    });
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete User', async() => {
+    const res = await request(app).delete('/api/user/delete').set('Cookie', [`token=${token}`]);
+    expect(res.status).toBe(200);
+  });
+});
+
+describe('Expense Tracker API', () => {
+  let expenseId;
+  test('Add Expense', async () => {
+    const register = await request(app).post('/api/user/register').send({
+      "name": "add user test",
+      "email": "test@test.com",
+      "password": "test"
+    });
+    const login = await request(app).post('/api/user/login').send(
+    {
+      "email": "test@test.com",
+      "password": "test"
+    });
+    token = login.body.token;
+    const res = await request(app).post('/api/expense/add').set('Cookie', [`token=${token}`]).send(
+      {
+        "type": "income",
+        "amount": 100000,
+        "description": "Dari langit"
+      }
+    );
+    expect(login.status).toBe(200);
+  });
+
+  test('List Expense', async () => {
+    const res = await request(app).get('/api/expense').set('Cookie', [`token=${token}`]);
+    expenseId = res.body.data[0].id;
+    expect(res.status).toBe(200);
+  });
+
+  test('Edit Expense', async () => {
+    const res = await request(app).put(`/api/expense/edit/${expenseId}`).set('Cookie', [`token=${token}`]).send(
+      {
+        "description": "Bayaran joki tugas"
+      }
+    );
+    expect(res.status).toBe(200);
+  });
+
+  test('Delete Expense', async () => {
+    const res = await request(app).delete(`/api/expense/delete/${expenseId}`).set('Cookie', [`token=${token}`]);
+    const deleteUser = await request(app).delete('/api/user/delete').set('Cookie', [`token=${token}`]);
+    expect(res.status).toBe(200);
+  });
+
+})
---
+++ tests/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const routes = require('../routes');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', async (req, res) => {
+  res.send({
+    message: 'You are using Expense Tracker by Kelompok 4'
+  });
+});
+
+routes(app);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app
